f319c78a44b1de5244adda5334ce29074c43cfaa
Pritiranjan Mishra <mishrapritiranjan3@gmail.com>
2021-10-06 09:07:01 +0530

    Changes made

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const xl = require('excel4node');
 const wb = new xl.Workbook();
 const ws = wb.addWorksheet("output");
 var finalData = [];
+var frequencyData = [];
 
 // Convert Excel To Json
-const convertExcelToJson = () => {
-    return new Promise((resolve, reject) => {
+const convertInputExcelToJson = () => {
+    return new Promise(async (resolve, reject) => {
         xlsxj({
             input: "input.xlsx",
             output: "output.json"
@@ -15,88 +16,95 @@ const convertExcelToJson = () => {
                 console.error(err);
                 reject(err)
             } else {
-
+                console.log("Running the script ")
                 outputData = result;
-
-                // Count the occurance of genus
-
                 let rez = []
                 result.forEach(function (item) {
                     rez[item.genus] ? rez[item.genus]++ : rez[item.genus] = 1;
                 });
 
-                /*
-                * Loop through the data to  find the occurance
-                * Find the first digit of the occurance count
-                * If occurance count is 93 then takecount will be the first letter i.e 9
-                * If occurance is single digit like 4 then take count will be the 1
-                * Take count is the final count for the number of data to be needs to be written into the output file
-                */
-                for (var key in rez) {
-                    if (rez.hasOwnProperty(key)) {
+                xlsxj({
+                    input: "frq_repr.xlsx",
+                    output: "outputFrq.json"
+                }, function (err1, result1) {
+                    if (err1) {
+                        console.log(err1)
+                        reject(err1)
+                    } else {
+                        /*
+* Loop through the data to  find the occurance
+* Find the first digit of the occurance count
+* If occurance count is 93 then takecount will be the first letter i.e 9
+* If occurance is single digit like 4 then take count will be the 1
+* Take count is the final count for the number of data to be needs to be written into the output file
+*/
 
-                        let takeCount = 0;
-                        if (rez[key] < 10) {
 
-                            takeCount = 1;
+                        frequencyData = result1;
+                        for (var key in rez) {
+                            if (rez.hasOwnProperty(key)) {
+                                let takeCount = 0;
+                                // console.log("=++++")
+                                var filtered = frequencyData.filter(function (el) {
 
-                        } else if (rez[key] > 10 && rez[key] < 99) {
-                            takeCount = parseInt(('' + rez[key]).slice(0, 1))
+                                    return el.genus === key;
+                                });
 
-                        } else if (rez[key] > 99 && rez[key] < 999) {
-                            takeCount = parseInt(('' + rez[key]).slice(0, 2))
+                                let filterData = result.filter(function (ele) {
+
+                                    return ele.genus == key
+                                })
+                                takeCount = (filtered[0]['selected'])
+                                for (let i = 0; i < takeCount; i++) {
+                                    finalData.push(filterData[i])
+                                }
+
+
+
+                            }
 
                         }
 
-                        let filterData = result.filter(function (ele) {
+                        var data = finalData;
+                        const headingColumnNames = Object.keys(data[0]);
+                        let headingColumnIndex = 1;
+                        headingColumnNames.forEach(heading => {
 
-                            return ele.genus == key
+                            ws.cell(1, headingColumnIndex++)
+                                .string(heading)
+
+
                         })
-                        // console.log("Filter data count --", filterData.length + "   take count ---  " + takeCount + " -----key-----> " + key);
-                        for (let i = 0; i < takeCount; i++) {
-                            finalData.push(filterData[i])
-                        }
+
+                        //Write Data in Excel file
+                        let rowIndex = 2;
+                        data.forEach(record => {
+
+                            let columnIndex = 1;
+
+                            Object.keys(record).forEach(columnName => {
+
+                                ws.cell(rowIndex, columnIndex++)
+                                    .string(record[columnName])
+                            });
+                            rowIndex++;
+                        });
+                        console.log("Writing data to excel ...")
+                        wb.write("output.xlsx");
+                        console.log("Check output.xlsx file for output")
+
+                        resolve()
+
                     }
-                }
-                data = finalData.filter(function (element) {
-                    return element !== undefined;
-                });
-
-                const headingColumnNames = Object.keys(data[0]);
-
-                //Write Column Title in Excel file
-                let headingColumnIndex = 1;
-
-                headingColumnNames.forEach(heading => {
-
-                    ws.cell(1, headingColumnIndex++)
-                        .string(heading)
-
 
                 })
 
-                //Write Data in Excel file
-                let rowIndex = 2;
 
-                data.forEach(record => {
 
-                    let columnIndex = 1;
 
-                    Object.keys(record).forEach(columnName => {
-
-                        ws.cell(rowIndex, columnIndex++)
-                            .string(record[columnName])
-                    });
-                    rowIndex++;
-                });
-                wb.write("output.xlsx");
-
-                // console.log(data)
-                resolve(rez)
             }
-        });
+        })
     })
-
 }
 
-convertExcelToJson()
+convertInputExcelToJson()
